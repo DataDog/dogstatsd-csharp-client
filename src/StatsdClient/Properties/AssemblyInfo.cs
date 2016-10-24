72974b617637db290bf2b55b159837ca5f6cd1c7
Yann MAHE <yann@datadoghq.com>
2016-10-24 14:10:22 -0400

    [ci] patch assembly info
    Setup AppVeyor AssemblyInfo patching: use `appveyor.yml` as a single
    source of truth for the NuGet package version.

--- src/StatsdClient/Properties/AssemblyInfo.cs
+++ src/StatsdClient/Properties/AssemblyInfo.cs
@@ -2,7 +2,7 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
+// General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("DogStatsD for C#")]
@@ -14,13 +14,13 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("ecebfa48-5557-4fe6-84a6-c0b1e3ece14c")]
 
-// This project follows http://semver.org/ for versioning
-[assembly: AssemblyVersion("1.0.0")]
+[assembly: AssemblyVersion("0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0")]
