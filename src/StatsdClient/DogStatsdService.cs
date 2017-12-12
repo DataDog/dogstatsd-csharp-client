86c3d0309874f3b856afd966c1032e3c779c9952
Patrick Purcell <patrick@sinch.com>
2017-12-12 15:59:28 +0100

    Use null propagation in DogStatsdService

--- src/StatsdClient/DogStatsdService.cs
+++ src/StatsdClient/DogStatsdService.cs
@@ -29,76 +29,42 @@ namespace StatsdClient
 
         public void Event(string title, string text, string alertType = null, string aggregationKey = null, string sourceType = null, int? dateHappened = null, string priority = null, string hostname = null, string[] tags = null)
         {
-            if (_statsD == null)
-            {
-                return;
-            }
-            _statsD.Send(title, text, alertType, aggregationKey, sourceType, dateHappened, priority, hostname, tags);
+            _statsD?.Send(title, text, alertType, aggregationKey, sourceType, dateHappened, priority, hostname, tags);
         }
-
 
         public void Counter<T>(string statName, T value, double sampleRate = 1.0, string[] tags = null)
         {
-            if (_statsD == null)
-            {
-                return;
-            }
-            _statsD.Send<Statsd.Counting, T>(BuildNamespacedStatName(statName), value, sampleRate, tags);
+            _statsD?.Send<Statsd.Counting, T>(BuildNamespacedStatName(statName), value, sampleRate, tags);
         }
 
         public void Increment(string statName, int value = 1, double sampleRate = 1.0, string[] tags = null)
         {
-            if (_statsD == null)
-            {
-                return;
-            }
-            _statsD.Send<Statsd.Counting, int>(BuildNamespacedStatName(statName), value, sampleRate, tags);
+            _statsD?.Send<Statsd.Counting, int>(BuildNamespacedStatName(statName), value, sampleRate, tags);
         }
 
         public void Decrement(string statName, int value = 1, double sampleRate = 1.0, string[] tags = null)
         {
-            if (_statsD == null)
-            {
-                return;
-            }
-            _statsD.Send<Statsd.Counting, int>(BuildNamespacedStatName(statName), -value, sampleRate, tags);
+            _statsD?.Send<Statsd.Counting, int>(BuildNamespacedStatName(statName), -value, sampleRate, tags);
         }
 
         public void Gauge<T>(string statName, T value, double sampleRate = 1.0, string[] tags = null)
         {
-            if (_statsD == null)
-            {
-                return;
-            }
-            _statsD.Send<Statsd.Gauge, T>(BuildNamespacedStatName(statName), value, sampleRate, tags);
+            _statsD?.Send<Statsd.Gauge, T>(BuildNamespacedStatName(statName), value, sampleRate, tags);
         }
 
         public void Histogram<T>(string statName, T value, double sampleRate = 1.0, string[] tags = null)
         {
-            if (_statsD == null)
-            {
-                return;
-            }
-            _statsD.Send<Statsd.Histogram, T>(BuildNamespacedStatName(statName), value, sampleRate, tags);
+            _statsD?.Send<Statsd.Histogram, T>(BuildNamespacedStatName(statName), value, sampleRate, tags);
         }
 
         public void Set<T>(string statName, T value, double sampleRate = 1.0, string[] tags = null)
         {
-            if (_statsD == null)
-            {
-                return;
-            }
-            _statsD.Send<Statsd.Set, T>(BuildNamespacedStatName(statName), value, sampleRate, tags);
+            _statsD?.Send<Statsd.Set, T>(BuildNamespacedStatName(statName), value, sampleRate, tags);
         }
 
         public void Timer<T>(string statName, T value, double sampleRate = 1.0, string[] tags = null)
         {
-            if (_statsD == null)
-            {
-                return;
-            }
-
-            _statsD.Send<Statsd.Timing, T>(BuildNamespacedStatName(statName), value, sampleRate, tags);
+            _statsD?.Send<Statsd.Timing, T>(BuildNamespacedStatName(statName), value, sampleRate, tags);
         }
 
 
@@ -134,12 +100,7 @@ namespace StatsdClient
 
         public void ServiceCheck(string name, Status status, int? timestamp = null, string hostname = null, string[] tags = null, string message = null)
         {
-            if (_statsD == null)
-            {
-                return;
-            }
-
-            _statsD.Send(name, (int)status, timestamp, hostname, tags, message);
+            _statsD?.Send(name, (int)status, timestamp, hostname, tags, message);
         }
 
         private string BuildNamespacedStatName(string statName)
