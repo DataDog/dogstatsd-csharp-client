4cb80547146cfb498cbf331e4b3ae59af0b55255
Nathan Johnstone <locotewz@gmail.com>
2018-09-20 19:23:18 +0200

    Migrate test to use instance based DogstatsdService rather than static singleton interface

--- tests/StatsdClient.Tests/StatsdConfigurationTests.cs
+++ tests/StatsdClient.Tests/StatsdConfigurationTests.cs
@@ -11,65 +11,75 @@ namespace Tests
     public class StatsdConfigurationTest
     {
         private void testReceive(string testServerName, int testPort, string testCounterName,
-                                 string expectedOutput)
+            string expectedOutput, DogStatsdService dogStatsdService)
         {
             UdpListener udpListener = new UdpListener(testServerName, testPort);
             Thread listenThread = new Thread(new ParameterizedThreadStart(udpListener.Listen));
             listenThread.Start();
-            DogStatsd.Increment(testCounterName);
+            dogStatsdService.Increment(testCounterName);
             while (listenThread.IsAlive) ;
             Assert.AreEqual(expectedOutput, udpListener.GetAndClearLastMessages()[0]);
             udpListener.Dispose();
         }
 
+        private DogStatsdService CreateSut()
+        {
+            return new DogStatsdService();
+        }
+
         [Test]
         public void throw_exception_when_no_config_provided()
         {
+            var sut = CreateSut();
             StatsdConfig metricsConfig = null;
-            Assert.Throws<ArgumentNullException>(() => StatsdClient.DogStatsd.Configure(metricsConfig));
+            Assert.Throws<ArgumentNullException>(() => sut.Configure(metricsConfig));
         }
 
         [Test]
         public void throw_exception_when_no_hostname_provided()
         {
+            var sut = CreateSut();
             var metricsConfig = new StatsdConfig { };
-            Assert.Throws<ArgumentNullException>(() => StatsdClient.DogStatsd.Configure(metricsConfig));
+            Assert.Throws<ArgumentNullException>(() => sut.Configure(metricsConfig));
         }
 
         [Test]
         public void default_port_is_8125()
         {
+            var sut = CreateSut();
             var metricsConfig = new StatsdConfig
             {
                 StatsdServerName = "127.0.0.1"
             };
-            StatsdClient.DogStatsd.Configure(metricsConfig);
-            testReceive("127.0.0.1", 8125, "test", "test:1|c");
+            sut.Configure(metricsConfig);
+            testReceive("127.0.0.1", 8125, "test", "test:1|c", sut);
         }
 
         [Test]
         public void setting_port()
         {
+            var sut = CreateSut();
             var metricsConfig = new StatsdConfig
             {
                 StatsdServerName = "127.0.0.1",
                 StatsdPort = 8126
             };
-            StatsdClient.DogStatsd.Configure(metricsConfig);
-            testReceive("127.0.0.1", 8126, "test", "test:1|c");
+            sut.Configure(metricsConfig);
+            testReceive("127.0.0.1", 8126, "test", "test:1|c", sut);
         }
 
         [Test]
         public void setting_prefix()
         {
+            var sut = CreateSut();
             var metricsConfig = new StatsdConfig
             {
                 StatsdServerName = "127.0.0.1",
                 StatsdPort = 8129,
                 Prefix = "prefix"
             };
-            StatsdClient.DogStatsd.Configure(metricsConfig);
-            testReceive("127.0.0.1", 8129, "test", "prefix.test:1|c");
+            sut.Configure(metricsConfig);
+            testReceive("127.0.0.1", 8129, "test", "prefix.test:1|c", sut);
         }
     }
 }
