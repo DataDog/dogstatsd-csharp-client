53864cc4bec3bed8b7fe7e37bda1fd7a9bb64080
Olivier Gaça <olivier.gaca@datadoghq.com>
2019-11-28 14:17:18 +0100

    Prepare the move of SendAsync from StatsdUDP to SocketSender (To have a better diff in next commit)

--- src/StatsdClient/SocketSender.cs
+++ src/StatsdClient/SocketSender.cs
@@ -48,6 +48,64 @@ namespace StatsdClient
                 }
             }
             sender(encodedCommand);
-        }        
+        }
+
+        //private async Task SendAsync(ArraySegment<byte> encodedCommand)
+        //{
+        //    if (MaxUDPPacketSize > 0 && encodedCommand.Count > MaxUDPPacketSize)
+        //    {
+        //        // If the command is too big to send, linear search backwards from the maximum
+        //        // packet size (taking into account the offset in the array)
+        //        // to see if we can find a newline delimiting two stats. If we can,
+        //        // split the message across the newline and try sending both componenets individually
+        //        byte newline = Encoding.UTF8.GetBytes("\n")[0];
+        //        for (int i = MaxUDPPacketSize + encodedCommand.Offset; i > encodedCommand.Offset; i--)
+        //        {
+        //            if (encodedCommand.Array[i] == newline)
+        //            {
+        //                var encodedCommandFirst = new ArraySegment<byte>(encodedCommand.Array, encodedCommand.Offset, i);
+
+        //                await SendAsync(encodedCommandFirst).ConfigureAwait(false);
+
+        //                int remainingCharacters = encodedCommand.Count - i - 1;
+        //                if (remainingCharacters > 0)
+        //                {
+        //                    await SendAsync(new ArraySegment<byte>(encodedCommand.Array, i + 1, remainingCharacters)).ConfigureAwait(false);
+        //                }
+
+        //                return; // We're done here if we were able to split the message.
+        //            }
+        //            // At this point we found an oversized message but we weren't able to find a
+        //            // newline to split upon. We'll still send it to the UDP socket, which upon sending an oversized message
+        //            // will fail silently if the user is running in release mode or report a SocketException if the user is
+        //            // running in debug mode.
+        //            // Since we're conservative with our MAX_UDP_PACKET_SIZE, the oversized message might even
+        //            // be sent without issue.
+        //        }
+        //    }
+        //    var tcs = new TaskCompletionSource<object>();
+
+        //    var args = new SocketAsyncEventArgs
+        //    {
+        //        RemoteEndPoint = IPEndpoint,
+        //        SocketFlags = SocketFlags.None,
+        //    };
+        //    args.SetBuffer(encodedCommand.Array, encodedCommand.Offset, encodedCommand.Count);
+        //    args.Completed += new EventHandler<SocketAsyncEventArgs>((object sender, SocketAsyncEventArgs eventArgs) => {
+        //        if (eventArgs.SocketError == SocketError.Success)
+        //        {
+        //            tcs.SetResult(null);
+        //        }
+        //        else
+        //        {
+        //            tcs.SetException(new SocketException((int)eventArgs.SocketError));
+        //        }
+        //    });
+        //    var completedAsync = UDPSocket.SendToAsync(args);
+        //    if (!completedAsync)
+        //        tcs.SetResult(null);
+
+        //    await tcs.Task;
+        //}
     }
 }
