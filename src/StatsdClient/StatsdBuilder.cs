7f6758530188e414fc1667b086b60b628e6862d9
Olivier Gaça <olivier.gaca@datadoghq.com>
2020-03-25 16:41:11 +0100

    Refactor StatsdBuilder.BuildStats

--- src/StatsdClient/StatsdBuilder.cs
+++ src/StatsdClient/StatsdBuilder.cs
@@ -5,7 +5,7 @@ namespace StatsdClient
 {
     class StatsdBuilder : IDisposable
     {
-        readonly List<IDisposable> _disposables = new List<IDisposable>();        
+        readonly List<IDisposable> _disposables = new List<IDisposable>();
 
         public Statsd BuildStats(StatsdConfig config)
         {
@@ -13,30 +13,35 @@ namespace StatsdClient
                     ? config.StatsdServerName
                     : Environment.GetEnvironmentVariable(StatsdConfig.DD_AGENT_HOST_ENV_VAR);
 
-            if (!string.IsNullOrEmpty(statsdServerName))
+            if (string.IsNullOrEmpty(statsdServerName))
             {
-                IStatsdUDP statsdSender;
+                throw new ArgumentNullException(
+                    $"{nameof(config)}.{nameof(config.StatsdServerName)} and" 
+                    + $" {StatsdConfig.DD_AGENT_HOST_ENV_VAR} environment variable not set");
+            }
 
-                if (statsdServerName.StartsWith(StatsdUnixDomainSocket.UnixDomainSocketPrefix))
-                {
-                    var statsdUds = new StatsdUnixDomainSocket(statsdServerName, config.StatsdMaxUnixDomainSocketPacketSize);
-                    _disposables.Add(statsdUds);
-                    statsdSender = statsdUds;
-                }
-                else
-                {
-                    var statsUdp = new StatsdUDP(config.StatsdServerName, config.StatsdPort, config.StatsdMaxUDPPacketSize);
-                    _disposables.Add(statsUdp);
-                    statsdSender = statsUdp;
-                }
-                var statsD = new Statsd(statsdSender, new RandomGenerator(), new StopWatchFactory(), "", config.ConstantTags);
-                statsD.TruncateIfTooLong = config.StatsdTruncateIfTooLong;
-                return statsD;
-            }
-            else
+            var statsdSender = CreateSender(config, statsdServerName);
+            var statsD = new Statsd(statsdSender,
+                                    new RandomGenerator(),
+                                    new StopWatchFactory(),
+                                    "",
+                                    config.ConstantTags);
+            statsD.TruncateIfTooLong = config.StatsdTruncateIfTooLong;
+            return statsD;
+        }
+
+        IStatsdUDP CreateSender(StatsdConfig config, string statsdServerName)
+        {
+            if (statsdServerName.StartsWith(StatsdUnixDomainSocket.UnixDomainSocketPrefix))
             {
-                throw new ArgumentNullException("config.StatsdServername and DD_AGENT_HOST environment variable not set");
+                var statsdUds = new StatsdUnixDomainSocket(statsdServerName, config.StatsdMaxUnixDomainSocketPacketSize);
+                _disposables.Add(statsdUds);
+                return statsdUds;
             }
+
+            var statsUdp = new StatsdUDP(config.StatsdServerName, config.StatsdPort, config.StatsdMaxUDPPacketSize);
+            _disposables.Add(statsUdp);
+            return statsUdp;
         }
 
         public void Dispose()
