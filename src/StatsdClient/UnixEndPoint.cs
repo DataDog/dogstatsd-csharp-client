95803e107cc5b5c94e634785373e03b6aebcb06e
Olivier Gaça <olivier.gaca@datadoghq.com>
2020-04-06 17:15:46 +0200

    Fix style for UnixEndPoint.cs including mix between tabs and spaces.

--- src/StatsdClient/UnixEndPoint.cs
+++ src/StatsdClient/UnixEndPoint.cs
@@ -4,7 +4,7 @@
 // Mono.Unix.UnixEndPoint: EndPoint derived class for AF_UNIX family sockets.
 //
 // Authors:
-//	Gonzalo Paniagua Javier (gonzalo@ximian.com)
+// Gonzalo Paniagua Javier (gonzalo@ximian.com)
 //
 // (C) 2003 Ximian, Inc (http://www.ximian.com)
 //
@@ -17,10 +17,10 @@
 // distribute, sublicense, and/or sell copies of the Software, and to
 // permit persons to whom the Software is furnished to do so, subject to
 // the following conditions:
-// 
+//
 // The above copyright notice and this permission notice shall be
 // included in all copies or substantial portions of the Software.
-// 
+//
 // THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
 // EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
 // MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
@@ -36,98 +36,117 @@ using System.Text;
 
 namespace Mono.Unix
 {
-    class UnixEndPoint : EndPoint
-	{
-		string filename;
-		
-		public UnixEndPoint (string filename)
-		{
-			if (filename == null)
-				throw new ArgumentNullException ("filename");
+    internal class UnixEndPoint : EndPoint
+    {
+        private string filename;
 
-			if (filename == "")
-				throw new ArgumentException ("Cannot be empty.", "filename");
-			this.filename = filename;
-		}
-		
-		public string Filename {
-			get {
-				return(filename);
-			}
-			set {
-				filename=value;
-			}
-		}
+        public UnixEndPoint(string filename)
+        {
+            if (filename == null)
+            {
+                throw new ArgumentNullException("filename");
+            }
 
-		public override AddressFamily AddressFamily {
-			get { return AddressFamily.Unix; }
-		}
+            if (filename == string.Empty)
+            {
+                throw new ArgumentException("Cannot be empty.", "filename");
+            }
 
-		public override EndPoint Create (SocketAddress socketAddress)
-		{
-			/*
-			 * Should also check this
-			 *
-			int addr = (int) AddressFamily.Unix;
-			if (socketAddress [0] != (addr & 0xFF))
-				throw new ArgumentException ("socketAddress is not a unix socket address.");
+            this.filename = filename;
+        }
 
-			if (socketAddress [1] != ((addr & 0xFF00) >> 8))
-				throw new ArgumentException ("socketAddress is not a unix socket address.");
-			 */
+        public string Filename
+        {
+            get
+            {
+                return (filename);
+            }
 
-			if (socketAddress.Size == 2) {
-				// Empty filename.
-				// Probably from RemoteEndPoint which on linux does not return the file name.
-				UnixEndPoint uep = new UnixEndPoint ("a");
-				uep.filename = "";
-				return uep;
-			}
-			int size = socketAddress.Size - 2;
-			byte [] bytes = new byte [size];
-			for (int i = 0; i < bytes.Length; i++) {
-				bytes [i] = socketAddress [i + 2];
-				// There may be junk after the null terminator, so ignore it all.
-				if (bytes [i] == 0) {
-					size = i;
-					break;
-				}
-			}
+            set
+            {
+                filename = value;
+            }
+        }
 
-			string name = Encoding.UTF8.GetString (bytes, 0, size);
-			return new UnixEndPoint (name);
-		}
+        public override AddressFamily AddressFamily
+        {
+            get { return AddressFamily.Unix; }
+        }
 
-		public override SocketAddress Serialize ()
-		{
-			byte [] bytes = Encoding.UTF8.GetBytes (filename);
-			SocketAddress sa = new SocketAddress (AddressFamily, 2 + bytes.Length + 1);
-			// sa [0] -> family low byte, sa [1] -> family high byte
-			for (int i = 0; i < bytes.Length; i++)
-				sa [2 + i] = bytes [i];
+        public override EndPoint Create(SocketAddress socketAddress)
+        {
+            /*
+             * Should also check this
+             *
+            int addr = (int) AddressFamily.Unix;
+            if (socketAddress [0] != (addr & 0xFF))
+                throw new ArgumentException ("socketAddress is not a unix socket address.");
 
-			//NULL suffix for non-abstract path
-			sa[2 + bytes.Length] = 0;
+            if (socketAddress [1] != ((addr & 0xFF00) >> 8))
+                throw new ArgumentException ("socketAddress is not a unix socket address.");
+             */
 
-			return sa;
-		}
+            if (socketAddress.Size == 2)
+            {
+                // Empty filename.
+                // Probably from RemoteEndPoint which on linux does not return the file name.
+                UnixEndPoint uep = new UnixEndPoint("a");
+                uep.filename = string.Empty;
+                return uep;
+            }
 
-		public override string ToString() {
-			return(filename);
-		}
+            int size = socketAddress.Size - 2;
+            byte[] bytes = new byte[size];
+            for (int i = 0; i < bytes.Length; i++)
+            {
+                bytes[i] = socketAddress[i + 2];
+                // There may be junk after the null terminator, so ignore it all.
+                if (bytes[i] == 0)
+                {
+                    size = i;
+                    break;
+                }
+            }
 
-		public override int GetHashCode ()
-		{
-			return filename.GetHashCode ();
-		}
+            string name = Encoding.UTF8.GetString(bytes, 0, size);
+            return new UnixEndPoint(name);
+        }
 
-		public override bool Equals (object o)
-		{
-			UnixEndPoint other = o as UnixEndPoint;
-			if (other == null)
-				return false;
+        public override SocketAddress Serialize()
+        {
+            byte[] bytes = Encoding.UTF8.GetBytes(filename);
+            SocketAddress sa = new SocketAddress(AddressFamily, 2 + bytes.Length + 1);
+            // sa [0] -> family low byte, sa [1] -> family high byte
+            for (int i = 0; i < bytes.Length; i++)
+            {
+                sa[2 + i] = bytes[i];
+            }
 
-			return (other.filename == filename);
-		}
-	}
+            // NULL suffix for non-abstract path
+            sa[2 + bytes.Length] = 0;
+
+            return sa;
+        }
+
+        public override string ToString()
+        {
+            return (filename);
+        }
+
+        public override int GetHashCode()
+        {
+            return filename.GetHashCode();
+        }
+
+        public override bool Equals(object o)
+        {
+            UnixEndPoint other = o as UnixEndPoint;
+            if (other == null)
+            {
+                return false;
+            }
+
+            return (other.filename == filename);
+        }
+    }
 }
