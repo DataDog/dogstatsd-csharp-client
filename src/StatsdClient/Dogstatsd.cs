79957d0b7f5c3d4a401e9c60e168a0208ed63ccc
Olivier Gaça <olivier.gaca@datadoghq.com>
2020-04-06 17:45:29 +0200

    Fix style for Dogstatsd.cs

--- src/StatsdClient/Dogstatsd.cs
+++ src/StatsdClient/Dogstatsd.cs
@@ -11,7 +11,7 @@ namespace StatsdClient
         OK = 0,
         WARNING = 1,
         CRITICAL = 2,
-        UNKNOWN = 3
+        UNKNOWN = 3,
     }
 
     public static class DogStatsd
@@ -20,12 +20,27 @@ namespace StatsdClient
 
         public static void Configure(StatsdConfig config) => _dogStatsdService.Configure(config);
 
-        public static void Event(string title, string text, string alertType = null, string aggregationKey = null,
-            string sourceType = null, int? dateHappened = null, string priority = null, string hostname = null,
+        public static void Event(
+            string title,
+            string text,
+            string alertType = null,
+            string aggregationKey = null,
+            string sourceType = null,
+            int? dateHappened = null,
+            string priority = null,
+            string hostname = null,
             string[] tags = null)
             =>
-                _dogStatsdService.Event(title: title, text: text, alertType: alertType, aggregationKey: aggregationKey, sourceType: sourceType, dateHappened: dateHappened, priority: priority,
-                    hostname: hostname, tags: tags);
+                _dogStatsdService.Event(
+                    title: title,
+                    text: text,
+                    alertType: alertType,
+                    aggregationKey: aggregationKey,
+                    sourceType: sourceType,
+                    dateHappened: dateHappened,
+                    priority: priority,
+                    hostname: hostname,
+                    tags: tags);
 
         public static void Counter<T>(string statName, T value, double sampleRate = 1.0, string[] tags = null) =>
             _dogStatsdService.Counter<T>(statName: statName, value: value, sampleRate: sampleRate, tags: tags);
@@ -60,8 +75,13 @@ namespace StatsdClient
         public static T Time<T>(Func<T> func, string statName, double sampleRate = 1.0, string[] tags = null) =>
             _dogStatsdService.Time<T>(func: func, statName: statName, sampleRate: sampleRate, tags: tags);
 
-        public static void ServiceCheck(string name, Status status, int? timestamp = null, string hostname = null,
-            string[] tags = null, string message = null) =>
+        public static void ServiceCheck(
+            string name,
+            Status status,
+            int? timestamp = null,
+            string hostname = null,
+            string[] tags = null,
+            string message = null) =>
                 _dogStatsdService.ServiceCheck(name, status, timestamp, hostname, tags, message);
 
         public static ITelemetryCounters TelemetryCounters => _dogStatsdService.TelemetryCounters;
