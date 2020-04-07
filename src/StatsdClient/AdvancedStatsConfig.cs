cea738cef69979366b0a9c3b396b020ea03a636b
Olivier G <olivier.gaca@datadoghq.com>
2020-04-07 19:53:34 +0200

    Set telemetry default interval to 10 seconds;

--- src/StatsdClient/AdvancedStatsConfig.cs
+++ src/StatsdClient/AdvancedStatsConfig.cs
@@ -40,7 +40,7 @@ namespace StatsdClient
         /// This value defines the duration between two telemetry flushes.
         /// When this value is set at null, telemetry is disabled.
         /// </summary>
-        public TimeSpan? TelemetryFlushInterval { get; set; } = TimeSpan.FromSeconds(30);
+        public TimeSpan? TelemetryFlushInterval { get; set; } = TimeSpan.FromSeconds(10);
 
         public AdvancedStatsConfig()
         {
