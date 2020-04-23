b9164da6efc2bb64409e545096912434b0dd939b
Olivier Gaça <olivier.gaca@datadoghq.com>
2020-04-23 13:14:27 +0200

    Simplify StatsBufferizeTests to avoid flakyness.

--- tests/StatsdClient.Tests/Bufferize/StatsBufferizeTests.cs
+++ tests/StatsdClient.Tests/Bufferize/StatsBufferizeTests.cs
@@ -3,7 +3,6 @@ using System.Text;
 using System.Threading.Tasks;
 using NUnit.Framework;
 using StatsdClient.Bufferize;
-using System.Threading;
 
 namespace Tests
 {
@@ -15,25 +14,14 @@ namespace Tests
         {
             var handler = new BufferBuilderHandlerMock();
             var bufferBuilder = new BufferBuilder(handler, 3, "\n");
-            var timeout = TimeSpan.FromMilliseconds(500);
-            using (var statsBufferize = new StatsBufferize(bufferBuilder, 10, null, timeout))
+            using (var statsBufferize = new StatsBufferize(bufferBuilder, 10, null, TimeSpan.Zero))
             {
-                var stopWatch = System.Diagnostics.Stopwatch.StartNew();
-                statsBufferize.Send("123");
-                statsBufferize.Send("4"); // Send "123" as "4" cannot be added to the current buffer.
+                statsBufferize.Send("1");
                 while (handler.Buffer == null)
                     Task.Delay(TimeSpan.FromMilliseconds(1)).Wait();
-                byte[] buffer = Interlocked.Exchange(ref handler.Buffer, null);
 
-                // Sent because buffer is full.
-                Assert.Less(stopWatch.ElapsedMilliseconds, timeout.TotalMilliseconds);
-                Assert.AreEqual("123", Encoding.UTF8.GetString(buffer));
-
-                while (handler.Buffer == null)
-                    Task.Delay(1).Wait();
-                // Sent because we wait more than the timeout.
-                Assert.Greater(stopWatch.ElapsedMilliseconds, timeout.TotalMilliseconds);
-                Assert.AreEqual("4", Encoding.UTF8.GetString(handler.Buffer));
+                // Sent because buffer is full.                
+                Assert.AreEqual("1", Encoding.UTF8.GetString(handler.Buffer));
             }
         }
     }
