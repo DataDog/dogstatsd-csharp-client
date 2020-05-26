2ef69d72de4bda44335cd28127472ccf11645e6b
Olivier G <olivier.gaca@datadoghq.com>
2020-05-26 12:09:18 +0200

    Fix case in EventSerializerTests

--- tests/StatsdClient.Tests/EventSerializerTests.cs
+++ tests/StatsdClient.Tests/EventSerializerTests.cs
@@ -9,55 +9,55 @@ namespace StatsdClient.Tests
     public class EventSerializerTests
     {
         [Test]
-        public void Send_event()
+        public void SendEvent()
         {
             AssertSerialize("_e{5,4}:title|text", "title", "text");
         }
 
         [Test]
-        public void Send_event_with_alertType()
+        public void SendEventWithAlertType()
         {
             AssertSerialize("_e{5,4}:title|text|t:warning", "title", "text", alertType: "warning");
         }
 
         [Test]
-        public void Send_event_with_aggregationKey()
+        public void SendEventWithAggregationKey()
         {
             AssertSerialize("_e{5,4}:title|text|k:key", "title", "text", aggregationKey: "key");
         }
 
         [Test]
-        public void Send_event_with_sourceType()
+        public void SendEventWithSourceType()
         {
             AssertSerialize("_e{5,4}:title|text|s:source", "title", "text", sourceType: "source");
         }
 
         [Test]
-        public void Send_event_with_dateHappened()
+        public void SendEventWithDateHappened()
         {
             AssertSerialize("_e{5,4}:title|text|d:123456", "title", "text", dateHappened: 123456);
         }
 
         [Test]
-        public void Send_event_with_priority()
+        public void SendEventWithPriority()
         {
             AssertSerialize("_e{5,4}:title|text|p:low", "title", "text", priority: "low");
         }
 
         [Test]
-        public void Send_event_with_hostname()
+        public void SendEventWithHostname()
         {
             AssertSerialize("_e{5,4}:title|text|h:hostname", "title", "text", hostname: "hostname");
         }
 
         [Test]
-        public void Send_event_with_tags()
+        public void SendEventWithTags()
         {
             AssertSerialize("_e{5,4}:title|text|#tag1,tag2", "title", "text", tags: new[] { "tag1", "tag2" });
         }
 
         [Test]
-        public void Send_event_with_message_that_is_too_long()
+        public void SendEventWithMessageThatIsTooLong()
         {
             var length = (8 * 1024) - 16; // 16 is the number of characters in the final message that is not the title
             var builder = BuildLongString(length);
@@ -70,7 +70,7 @@ namespace StatsdClient.Tests
         }
 
         [Test]
-        public void Send_event_with_truncation_for_title_that_is_too_long()
+        public void SendEventWithTruncationForTitleThatIsTooLong()
         {
             var length = (8 * 1024) - 16; // 16 is the number of characters in the final message that is not the title
             var builder = BuildLongString(length);
@@ -84,7 +84,7 @@ namespace StatsdClient.Tests
         }
 
         [Test]
-        public void Send_event_with_truncation_for_text_that_is_too_long()
+        public void SendEventWithTruncationForTextThatIsTooLong()
         {
             var length = (8 * 1024) - 17; // 17 is the number of characters in the final message that is not the text
             var builder = BuildLongString(length);
@@ -98,7 +98,7 @@ namespace StatsdClient.Tests
         }
 
         [Test]
-        public void Send_event_with_statsd_truncation()
+        public void SendEventWithStatsdTruncation()
         {
             var length = (8 * 1024) - 17; // 17 is the number of characters in the final message that is not the text
             var builder = BuildLongString(length);
