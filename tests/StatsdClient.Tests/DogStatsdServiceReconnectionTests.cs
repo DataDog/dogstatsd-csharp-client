2e2bd90dd2b11350d2e251c3ac72605373739019
Olivier G <olivier.gaca@datadoghq.com>
2020-05-28 16:01:06 +0200

    DogStatsdServiceReconnectionTests

---
+++ tests/StatsdClient.Tests/DogStatsdServiceReconnectionTests.cs
@@ -0,0 +1,66 @@
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using StatsdClient;
+using Tests.Utils;
+
+namespace Tests
+{
+    [TestFixture]
+    public class DogStatsdServiceReconnectionTests
+    {
+        [Test]
+        public void UDPReconnection()
+        {
+            CheckReconnection(new StatsdConfig
+            {
+                StatsdServerName = "127.0.0.1",
+                StatsdPort = 1234,
+            });
+        }
+
+        [Test]
+        public void UDSReconnection()
+        {
+            using (var temporaryPath = new TemporaryPath())
+            {
+                CheckReconnection(new StatsdConfig
+                {
+                    StatsdServerName = StatsdBuilder.UnixDomainSocketPrefix + temporaryPath.Path,
+                });
+            }
+        }
+
+        private static void CheckReconnection(StatsdConfig config)
+        {
+            SocketServer server = null;
+
+            try
+            {
+                server = new SocketServer(config);
+                using (var service = new DogStatsdService())
+                {
+                    service.Configure(config);
+                    service.Increment("test1");
+                    Assert.AreEqual("test1:1|c", server.Stop().Single());
+                    server.Dispose();
+
+                    // Send a metric when the server is not running.
+                    service.Increment("test2");
+                    Task.Delay(TimeSpan.FromMilliseconds(500)).Wait();
+
+                    // Restart the server
+                    server = new SocketServer(config, removeUDSFileBeforeStarting: true);
+                    service.Increment("test3");
+                    service.Dispose();
+                    Assert.AreEqual("test3:1|c", server.Stop().Last());
+                }
+            }
+            finally
+            {
+                server?.Dispose();
+            }
+        }
+    }
+}
