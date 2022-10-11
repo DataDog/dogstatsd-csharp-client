d71eb0557cd78313272b87050f04537f70580b9e
Remy Mathieu <remy.mathieu@datadoghq.com>
2022-10-11 11:22:37 +0200

    statsd: backport ToUnixTimeSeconds.

--- src/StatsdClient/MetricsSender.cs
+++ src/StatsdClient/MetricsSender.cs
@@ -89,7 +89,7 @@ namespace StatsdClient
                     }
                     else
                     {
-                        stats.Metric.Timestamp = timestamp.Value.ToUniversalTime().ToUnixTimeSeconds();
+                        stats.Metric.Timestamp = DateTimeOffsetHelper.ToUnixTimeSeconds(timestamp.Value);
                     }
 
                     Send(stats);
