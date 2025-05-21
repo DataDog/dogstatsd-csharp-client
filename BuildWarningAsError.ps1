5d7359adcffe67aa64bacc33af174f144dfc610f
Carlos Roman <carlos.roman@datadoghq.com>
2025-05-21 11:51:32 +0100

    Adding GitHub Action to run tests

--- BuildWarningAsError.ps1
+++ BuildWarningAsError.ps1
@@ -2,7 +2,9 @@
 # See https://github.com/dotnet/installer/issues/1708.
 
 # Ignore warning NETSDK1138: The target framework 'netcoreapp2.1' is out of support and will not receive security updates in the future
-dotnet build -warnaserror -warnAsMessage:NETSDK1138 *> build.txt
+# Ignore warning NU1902: The dependency Microsoft.NETCore.App 2.1.34 has a known high severity vulnerability (CVE-2021-43877) that we will ignore
+# Ignore warning NU1903: The dependency Microsoft.NETCore.App 2.1.34 has a known high severity vulnerability (CVE-2021-43877) that we will ignore
+dotnet build -warnaserror -warnAsMessage:NETSDK1138,NU1902,NU1903 *> build.txt
 Get-Content build.txt
 $noError = Select-String -Path build.txt -pattern "0 Error\(s\)"
 Exit $noError -eq $null
