eb5dc9d7e7c26857cb4d5beba5ecc660c4ef9b0c
Lucas Pimentel <lucaspimentel@gmail.com>
2025-11-26 00:02:18 -0500

    add native stat structs for macOS

--- src/StatsdClient/NativeMethods.cs
+++ src/StatsdClient/NativeMethods.cs
@@ -19,11 +19,24 @@ internal static class NativeMethods
     {
         try
         {
-            int result = Stat(path, out var statBuf);
-            if (result == 0)
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
             {
-                inode = statBuf.st_ino;
-                return true;
+                int result = StatMacOS(path, out var statBuf);
+                if (result == 0)
+                {
+                    inode = statBuf.st_ino;
+                    return true;
+                }
+            }
+            else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+            {
+                // Linux (and other Unix-like systems)
+                int result = StatLinux(path, out var statBuf);
+                if (result == 0)
+                {
+                    inode = statBuf.st_ino;
+                    return true;
+                }
             }
         }
         catch
@@ -36,65 +49,111 @@ internal static class NativeMethods
         return false;
     }
 
+    // Linux stat syscall (x86_64 and ARM64/aarch64)
     [DllImport("libc", SetLastError = true, EntryPoint = "stat", CharSet = CharSet.Ansi)]
-    private static extern int Stat(string pathname, out StatStruct buf);
+    private static extern int StatLinux(string pathname, out StatStructLinux buf);
 
+    // macOS stat syscall (x86_64 and ARM64/Apple Silicon)
+    [DllImport("libc", SetLastError = true, EntryPoint = "stat", CharSet = CharSet.Ansi)]
+    private static extern int StatMacOS(string pathname, out StatStructMacOS buf);
+
+    // Linux struct stat (asm-generic/stat.h - used by ARM64/aarch64 and x86_64)
+    // Reference: https://github.com/torvalds/linux/blob/master/include/uapi/asm-generic/stat.h
+    [StructLayout(LayoutKind.Sequential)]
+    private struct StatStructLinux
+    {
+        public ulong st_dev;        // Device
+        public ulong st_ino;        // File serial number (inode)
+        public uint st_mode;        // File mode
+        public uint st_nlink;       // Link count
+        public uint st_uid;         // User ID of the file's owner
+        public uint st_gid;         // Group ID of the file's group
+        public ulong st_rdev;       // Device number, if device
+        public ulong __pad1;
+        public long st_size;        // Size of file, in bytes
+        public int st_blksize;      // Optimal block size for I/O
+        public int __pad2;
+        public long st_blocks;      // Number 512-byte blocks allocated
+        public long st_atime;       // Time of last access
+        public ulong st_atime_nsec;
+        public long st_mtime;       // Time of last modification
+        public ulong st_mtime_nsec;
+        public long st_ctime;       // Time of last status change
+        public ulong st_ctime_nsec;
+        public uint __unused4;
+        public uint __unused5;
+    }
+
+    // macOS struct stat (sys/stat.h - x86_64 and ARM64)
+    // Reference: https://stackoverflow.com/questions/39671660
     [StructLayout(LayoutKind.Explicit, Size = 144)]
-    private struct StatStruct
+    private struct StatStructMacOS
     {
         [FieldOffset(0)]
-        public ulong st_dev;       // device (offset 0, 8 bytes)
+        public int st_dev;          // Device (4 bytes)
+
+        [FieldOffset(4)]
+        public ushort st_mode;      // File mode (2 bytes)
+
+        [FieldOffset(6)]
+        public ushort st_nlink;     // Link count (2 bytes)
 
         [FieldOffset(8)]
-        public ulong st_ino;       // inode (offset 8, 8 bytes)
+        public ulong st_ino;        // File serial number (inode) (8 bytes)
 
         [FieldOffset(16)]
-        public ulong st_nlink;     // number of hard links (offset 16, 8 bytes)
+        public uint st_uid;         // User ID (4 bytes)
+
+        [FieldOffset(20)]
+        public uint st_gid;         // Group ID (4 bytes)
 
         [FieldOffset(24)]
-        public uint st_mode;       // protection (offset 24, 4 bytes)
+        public int st_rdev;         // Device number (4 bytes)
 
-        [FieldOffset(28)]
-        public uint st_uid;        // user ID (offset 28, 4 bytes)
+        // Padding to align timespec at offset 32
+        // [FieldOffset(28)] - 4 bytes padding
 
+        // Four timespec structures (each 16 bytes: 8 bytes tv_sec + 8 bytes tv_nsec)
         [FieldOffset(32)]
-        public uint st_gid;        // group ID (offset 32, 4 bytes)
-
-        // [FieldOffset(36)] - 4 bytes padding
+        public long st_atimespec_sec;       // Access time seconds (8 bytes)
 
         [FieldOffset(40)]
-        public ulong st_rdev;      // device type (offset 40, 8 bytes)
+        public long st_atimespec_nsec;      // Access time nanoseconds (8 bytes)
 
         [FieldOffset(48)]
-        public long st_size;       // size (offset 48, 8 bytes)
+        public long st_mtimespec_sec;       // Modification time seconds (8 bytes)
 
         [FieldOffset(56)]
-        public long st_blksize;    // block size (offset 56, 8 bytes)
+        public long st_mtimespec_nsec;      // Modification time nanoseconds (8 bytes)
 
         [FieldOffset(64)]
-        public long st_blocks;     // blocks allocated (offset 64, 8 bytes)
+        public long st_ctimespec_sec;       // Status change time seconds (8 bytes)
 
         [FieldOffset(72)]
-        public long st_atime;      // access time (offset 72, 8 bytes)
+        public long st_ctimespec_nsec;      // Status change time nanoseconds (8 bytes)
 
         [FieldOffset(80)]
-        public long st_atime_nsec; // access time nsec (offset 80, 8 bytes)
+        public long st_birthtimespec_sec;   // Birth time seconds (8 bytes) - macOS specific
 
         [FieldOffset(88)]
-        public long st_mtime;      // modification time (offset 88, 8 bytes)
+        public long st_birthtimespec_nsec;  // Birth time nanoseconds (8 bytes) - macOS specific
 
         [FieldOffset(96)]
-        public long st_mtime_nsec; // modification time nsec (offset 96, 8 bytes)
+        public long st_size;                // File size in bytes (8 bytes)
 
         [FieldOffset(104)]
-        public long st_ctime;      // status change time (offset 104, 8 bytes)
+        public long st_blocks;              // Blocks allocated (8 bytes)
 
         [FieldOffset(112)]
-        public long st_ctime_nsec; // status change time nsec (offset 112, 8 bytes)
+        public int st_blksize;              // Optimal block size (4 bytes)
 
-        // Total size: 144 bytes (includes 24 bytes reserved at end on x86_64)
-        // Note: This struct layout matches the Linux x86_64 glibc struct stat layout.
-        // Using explicit offsets to ensure correct memory marshaling across architectures.
+        [FieldOffset(116)]
+        public uint st_flags;               // User defined flags - macOS specific (4 bytes)
+
+        [FieldOffset(120)]
+        public uint st_gen;                 // File generation number - macOS specific (4 bytes)
+
+        // Remaining fields up to 144 bytes
     }
 }
 
